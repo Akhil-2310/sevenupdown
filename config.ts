db917c8ed599670ec1309b857e10069818268456
Akhil Nanavati <akhilnanavati23@gmail.com>
2025-08-01 17:21:50 +0530

    hardhat and smart wallet config

--- config.ts
+++ config.ts
@@ -3,8 +3,9 @@ import {
   cookieStorage,
   createConfig,
 } from "@account-kit/react";
-import { alchemy, arbitrumSepolia } from "@account-kit/infra";
+import { alchemy, monadTestnet } from "@account-kit/infra";
 import { QueryClient } from "@tanstack/react-query";
+// import { monadTestnet } from "viem/chains";
 
 const API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
 if (!API_KEY) {
@@ -22,9 +23,7 @@ const uiConfig: AlchemyAccountsUIConfig = {
     sections: [
       [{ type: "email" }],
       [
-        { type: "passkey" },
         { type: "social", authProviderId: "google", mode: "popup" },
-        { type: "social", authProviderId: "facebook", mode: "popup" },
       ],
     ],
     addPasskeyOnSignup: false,
@@ -33,13 +32,13 @@ const uiConfig: AlchemyAccountsUIConfig = {
 
 export const config = createConfig(
   {
-    transport: alchemy({ apiKey: API_KEY }),
+    transport: alchemy({ apiKey: "kgzZuD1mIGAsUtjylIbm9" }),
     // Note: This quickstart is configured for Arbitrum Sepolia.
-    chain: arbitrumSepolia,
+    chain: monadTestnet,
     ssr: true, // more about ssr: https://www.alchemy.com/docs/wallets/react/ssr
     storage: cookieStorage, // more about persisting state with cookies: https://www.alchemy.com/docs/wallets/react/ssr#persisting-the-account-state
     enablePopupOauth: true, // must be set to "true" if you plan on using popup rather than redirect in the social login flow
-    policyId: SPONSORSHIP_POLICY_ID,
+    policyId: "28703310-c5f4-4bcf-bbf4-8a80c0cbbfe5",
   },
   uiConfig
 );
---
+++ hardhat.config.cjs
@@ -0,0 +1,16 @@
+const { monadTestnet } = require('viem/chains');
+
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition");
+
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+
+    monadTestnet: {
+      url: `https://monad-testnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
